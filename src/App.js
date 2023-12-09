6888587278d83025037a91d8c458f748c642965f
김우현 <rladngus133@naver.com>
2023-12-09 10:54:54 +0900

    build: CRA후 기본셋팅완료

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyles from "./GlobalStyles";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      {/* 여기 밑으로 컴포넌트 사용 */}
+    </>
+  );
+}
+
+export default App;
