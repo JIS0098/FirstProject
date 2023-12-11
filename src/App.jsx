b22d4508fd0cf8a26a0ebf5e65c4a2015780bc39
김우현 <rladngus133@naver.com>
2023-12-11 12:56:32 +0900

    build: 마지막 초기셋팅 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
-import Main from "./Main";
-import { theme } from "./styles/theme";
-import GlobalStyles from "./styles/GlobalStyles";
-
-function App() {
-  const [currentTheme, setCurrentTheme] = useState(theme["default"]);
-
-  const toggleTheme = () => {
-    const newTheme = currentTheme === theme["default"] ? "years" : "default";
-    setCurrentTheme(theme[newTheme]);
-  };
-
-  return (
-    <>
-      <ThemeProvider theme={currentTheme}>
-        <GlobalStyles />
-        <Button onClick={toggleTheme} />
-        {/* 여기 밑으로 컴포넌트 사용 */}
-        <div>header</div>
-        <Main />
-      </ThemeProvider>
-    </>
-  );
-}
-
-export default App;
-
-const Button = styled.button`
-  width: 200px;
-  height: 100px;
-  background-color: ${({ theme }) => theme.button.primary.enabled};
-  &:disabled {
-    background-color: ${({ theme }) => theme.button.primary.disabled};
-  }
-  &:hover {
-    background-color: ${({ theme }) => theme.button.primary.hover};
-  }
-`;
-=======
 import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import Main from "./Main";
@@ -67,4 +25,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 753ded51e1da88e2d541e6637c692211bb8a6992
