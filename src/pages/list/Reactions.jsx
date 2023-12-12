dfe28a49b357cc505bc4de4d4f62f563e4eab971
buguenkwon <guen9310@gmail.com>
2023-12-12 10:38:09 +0900

    chore : reactions 스타일 정렬

--- src/pages/list/Reactions.jsx
+++ src/pages/list/Reactions.jsx
@@ -12,19 +12,19 @@ export function Reactions({ emoji, count }) {
 const Badge = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   width: 6.5rem;
   height: 3.6rem;
-  font-size: 1.6rem;
   padding: 0.8rem 1.2rem;
   border-radius: 3.2rem;
-  justify-content: space-between;
   background: rgba(0, 0, 0, 0.54);
+  font-size: 1.6rem;
 `;
 
 const Count = styled.span`
   color: white;
   font-size: 1.6rem;
-  font-style: normal;
   font-weight: 400;
   line-height: 2rem;
+  font-style: normal;
 `;
