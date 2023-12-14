c9cae180535cf3f811ee57d5992f8c42419d1d0a
buguenkwon <guen9310@gmail.com>
2023-12-14 15:56:33 +0900

    chore : ProfileImages props  수정

--- src/components/commons/ProfileImages.jsx
+++ src/components/commons/ProfileImages.jsx
@@ -1,50 +1,57 @@
 import React from "react";
 import styled from "styled-components";
 
-function ProfileImgs({ list }) {
-  const leng = list.length - 3;
+function ProfileImgs({ list, count }) {
+  const leng = count - 3;
+  const isVisable = count > 3;
   return (
-    <ProfileImgBox>
-      <ProfileImg src={list[0].img} />
-      <ProfileImg src={list[1].img} />
-      <ProfileImg src={list[2].img} />
-      <ProfileDiv>+ {leng}</ProfileDiv>
-    </ProfileImgBox>
+    <ProfileBox>
+      <ProfileImg url={list[0]?.profileImageURL} />
+      <ProfileImg url={list[1]?.profileImageURL} />
+      <ProfileImg url={list[2]?.profileImageURL} />
+      <ProfileDiv $isVisible={isVisable}>+ {leng}</ProfileDiv>
+    </ProfileBox>
   );
 }
 
-const ProfileImgBox = styled.div`
+const ProfileBox = styled.div`
+  position: relative;
+  display: flex;
   min-width: 7.6rem;
   height: 2.8rem;
-  display: flex;
 `;
-const ProfileDiv = styled.div`
+
+const Profile = styled.div`
   width: 2.8rem;
   height: 2.8rem;
   background-color: #fff;
   border-radius: 100px;
   border: 1px solid white;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.2rem;
+  position: absolute;
 `;
-const ProfileImg = styled.img`
-  width: 2.8rem;
-  height: 2.8rem;
-  border-radius: 100px;
-  border: 1px solid white;
-  position: relative;
+
+const ProfileImg = styled(Profile)`
+  background: ${(props) => (props.url ? `url(${props.url})` : "gray")};
+  background-size: cover;
+  background-position: center;
 
   &:nth-child(1) {
-    left: 30px;
+    left: 0;
   }
   &:nth-child(2) {
     left: 20px;
   }
   &:nth-child(3) {
-    left: 10px;
+    left: 40px;
   }
 `;
+
+const ProfileDiv = styled(Profile)`
+  display: ${({ $isVisible }) => ($isVisible ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  left: 60px;
+`;
+
 export default ProfileImgs;
