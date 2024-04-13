6b5a688b34bd518b14864b9fde77a198727477f4
unknown <llqrw55@naver.com>
2024-04-13 20:59:05 +0900

    Feat:페이지별 Code Splitting

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Post from "../src/pages/post";
-import PostEdit from "../src/pages/postEdit";
-import ListPage from "./pages/list";
-import Landing from "pages/landing";
-import Header from "./components/commons/Header";
-import CreateMessage from "./pages/createMessage";
-import CreatePost from "./pages/createPost";
-import NotFound from "./pages/not_found/NotFound";
 import { LocationProvider } from "contexts/LocationContext";
 import { SnowTheme } from "styles/snowTheme";
+import CreateMessage from "pages/createMessage";
+
+const Landing = React.lazy(() => import("./pages/landing/index"));
+const ListPage = React.lazy(() => import("./pages/list/index"));
+const CreatePost = React.lazy(() => import("./pages/createPost/index"));
+const Post = React.lazy(() => import("./pages/post/index"));
+const PostEdit = React.lazy(() => import("./pages/postEdit/index"));
+const Header = React.lazy(() => import("./components/commons/Header"));
+const NotFound = React.lazy(() => import("./pages/not_found/NotFound"));
 
 function Main({ onToggleTheme, isDark }) {
   return (
     <BrowserRouter>
       <LocationProvider>
-        <Header onThemaClick={onToggleTheme} />
-        <SnowTheme isDark={isDark} />
-        <Routes>
-          <Route path="/">
-            <Route index element={<Landing />} />
-            <Route path="/list" element={<ListPage />} />
-          </Route>
-          <Route path="/post">
-            <Route index element={<CreatePost />} />
-            <Route path=":id" element={<Post />} />
-            <Route path=":id/edit" element={<PostEdit />} />
-            <Route path=":id/message" element={<CreateMessage />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header onThemaClick={onToggleTheme} />
+          <SnowTheme isDark={isDark} />
+          <Routes>
+            <Route path="/">
+              <Route index element={<Landing />} exact />
+              <Route path="/list" element={<ListPage />} exact />
+            </Route>
+            <Route path="/post">
+              <Route index element={<CreatePost />} exact />
+              <Route path=":id" element={<Post />} exact />
+              <Route path=":id/edit" element={<PostEdit />} exact />
+              <Route path=":id/message" element={<CreateMessage />} exact />
+            </Route>
+            <Route path="*" element={<NotFound />} exact />
+          </Routes>
+        </Suspense>
       </LocationProvider>
     </BrowserRouter>
   );
